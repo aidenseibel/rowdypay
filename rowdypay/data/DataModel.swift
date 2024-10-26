5e3a7e3b13ae5d0c9634cdc00df8633240570603
aidenseibel <aiden.seibel@gmail.com>
2024-10-26 13:39:46 -0500

    API connection set up

--- rowdypay/data/DataModel.swift
+++ rowdypay/data/DataModel.swift
@@ -9,34 +9,35 @@ import Foundation
 
 class DataModel {
     static func getHelloWorld() {
-        guard let url = URL(string: "https://localhost/9000/api/hello_world") else {
-            print("URL not found: https://localhost/9000/api/hello_world")
+        guard let url = URL(string: "https://e48f-129-115-2-245.ngrok-free.app/api/hello_world") else {
+            print("URL not found: https://e48f-129-115-2-245.ngrok-free.app/api/hello_world")
             return
         }
-        
+                
         var request = URLRequest(url: url)
         request.httpMethod = "GET"
         
         URLSession.shared.dataTask(with: request) { data, response, error in
             if let error = error {
-                print("Error: \(error)")
+                print("Error performing hello_world: \(error)")
                 return
             }
             
             print(response ?? "no response")
             guard let httpResponse = response as? HTTPURLResponse,
                   (200...299).contains(httpResponse.statusCode) else {
-                print("Server error")
+                print("Server error performing hello_world")
                 return
             }
 
             if let data = data {
                 if let responseString = String(data: data, encoding: .utf8) {
-                    print(responseString)
+                    print("Response from hello_world: "+responseString)
                 } else {
-                    print("Unable to convert data to string.")
+                    print("Unable to convert data to string when performing hello_world.")
                 }
             }
         }
+        .resume()
     }
 }
---
+++ rowdypay/views/components/GroupSubView.swift
@@ -0,0 +1,18 @@
+//
+//  GroupSubView.swift
+//  rowdypay
+//
+//  Created by Aiden Seibel on 10/26/24.
+//
+
+import SwiftUI
+
+struct GroupSubView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GroupSubView()
+}
