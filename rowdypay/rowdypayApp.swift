a88fb3ebaa214c9170565feccf4d7f78def9389e
aidenseibel <aiden.seibel@gmail.com>
2024-10-26 12:34:19 -0500

    Initial Commit

---
+++ rowdypay/rowdypayApp.swift
@@ -0,0 +1,17 @@
+//
+//  rowdypayApp.swift
+//  rowdypay
+//
+//  Created by Aiden Seibel on 10/26/24.
+//
+
+import SwiftUI
+
+@main
+struct rowdypayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
